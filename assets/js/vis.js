eb21a623defa44f89887fff22242c28f26e0f3c6
Steve Redka <cogitofallacy@gmail.com>
2018-01-21 20:24:42 +0300

    wip: timeline-scaling

--- assets/js/vis.js
+++ assets/js/vis.js
@@ -18,7 +18,16 @@ var options = {
       hour: "H"
     }
   },
-  onInitialDrawComplete: appendTimebarTimer
+  onInitialDrawComplete: appendTimebarTimer,
+  min: new Date('2015-12-15 6:00:00'),
+  // max: new Date('2015-12-15 23:00:00'),
+  // maxZoom: '1000 * 60 * 60 * 24',
+  // minZoom: '1000 * 60 * 60 * 24'
+  width: "1242px",
+  hiddenDates: [
+      {start: '2015-12-16 00:00:00', end: '2015-12-18 00:00:00', repeat: 'weekly'},
+      {start: '2015-12-15 22:00:00', end: '2013-03-30 06:00:00', repeat: 'daily'}
+  ],
 };
 
 // Create a Timeline
@@ -26,3 +35,9 @@ var timeline = new vis.Timeline(container, items, groups, options);
 
 var customDate = new Date('2015-12-15 11:05:00');
 timeline.addCustomTime(customDate, 'current-time-bar');
+
+timeline.on("rangechange", modifyLabelsetClass)
+
+function modifyLabelsetClass() {
+  console.log('ss')
+}
